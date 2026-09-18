7da1e59a05a5b80113b17e38335707b5ddd2155c
pedroay <75274984+pedroay@users.noreply.github.com>
2026-09-18 12:04:21 -0500

    feat: Create Administrator.java

---
+++ oficioya/src/main/java/edu/eci/dosw/oficioya/model/Administrator.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.net.URL;
+
+public class Administrator extends User {
+    // No tiene atributos propios definidos en el diagrama
+}
