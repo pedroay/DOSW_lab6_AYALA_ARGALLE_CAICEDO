7efd54cda7d181b29233f2ba2d7aa3024a1081d5
pedroay <75274984+pedroay@users.noreply.github.com>
2026-09-18 12:02:17 -0500

    feat: Create Employer.java

---
+++ oficioya/src/main/java/edu/eci/dosw/oficioya/model/Employer.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.net.URL;
+
+public class Employer extends User {
+    private int rate;
+    
+    private List<Request> requests;
+}
